e6a78bf2dd6749d108971974044d6b38bfa1e0f1
cta6ujiu3atop1 <marsel.melisov@iaau.edu.kg>
2019-03-21 10:18:53 +0600

    orders are finished

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -2,56 +2,77 @@
   <div class="container">
     <h1 class="title">Заказы</h1>
     <h2 class="subtitle">Активные</h2>
-    <div
-      class="box navbar"
-      v-for="(order, index) in getOrders"
-      :key="index"
+    <transition-group
+      name="list"
+      tag="div"
     >
-      <div class="navbar-brand navbar-item">
-        <p class="subtitle">{{index+1}}</p>
-      </div>
-      <div class="navbar-item">
-        <p class="title">{{order.name}}</p>
-      </div>
-      <div class="navbar-item">
-        <p class="subtitle"> <b>Телефон:</b> {{order.address}}</p>
-      </div>
-      <div class="navbar-item">
-        <p class="subtitle"><b>Адрес</b>: {{order.phone}}</p>
-      </div>
-      <div class="navbar-item">
-        <p class="subtitle">Заказ на сумму <b>{{order.total}}</b> сом</p>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <button class="button is-danger">Удалить</button>
+
+      <div
+        class="box navbar "
+        v-for="(order, index) in getOrders"
+        :key="index"
+      >
+        <div class="navbar-brand navbar-item">
+          <p class="subtitle">{{index+1}}</p>
         </div>
         <div class="navbar-item">
-          <button class="button is-primary">Подтвердить</button>
+          <p class="title">{{order.name}}</p>
+        </div>
+        <div class="navbar-item">
+          <p class="subtitle"> <b>Телефон:</b> {{order.phone}}</p>
+        </div>
+        <div class="navbar-item">
+          <p class="subtitle"><b>Адрес</b>: {{order.address}}</p>
+        </div>
+        <div class="navbar-item">
+          <p class="subtitle">Заказ на сумму <b>{{order.total}}</b> сом</p>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <button
+              class="button is-danger"
+              @click="deleteOrder(order)"
+            >Удалить</button>
+          </div>
+          <div class="navbar-item">
+            <button
+              class="button is-primary"
+              @click="confirmOrder(order)"
+            >Подтвердить</button>
+          </div>
         </div>
       </div>
-    </div>
+    </transition-group>
+
     <h2 class="subtitle">В процессе </h2>
-    <div
-      class="box navbar"
-      v-for="(order, index) in getOrdersInProcess"
-      :key="index"
+    <transition-group
+      name="list"
+      tag="div"
     >
-      <div class="navbar-item">
-        <p class="title">{{order.name}}</p>
-      </div>
-      <div class="navbar-item">
-        <p class="subtitle">{{order.address}}</p>
-      </div>
-      <div class="navbar-item">
-        <p class="subtitle">{{order.phone}}</p>
-      </div>
-      <div class="navbar-end">
+      <div
+        class="box navbar"
+        v-for="(order, index) in getOrdersInProcess"
+        :key="index"
+      >
         <div class="navbar-item">
-          <button class="button is-warning">Выполнен</button>
+          <p class="title">{{order.name}}</p>
+        </div>
+        <div class="navbar-item">
+          <p class="subtitle">{{order.address}}</p>
+        </div>
+        <div class="navbar-item">
+          <p class="subtitle">{{order.phone}}</p>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <button
+              class="button is-warning"
+              @click="finishOrder(order)"
+            >Выполнен</button>
+          </div>
         </div>
       </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
@@ -65,9 +86,44 @@ export default {
   },
   computed: {
     ...mapGetters(['getOrders', 'getOrdersInProcess'])
-  }
+  },
+  methods: {
+    deleteOrder (payload) {
+      this.$dialog.confirm({
+        title: 'Удаление заказа',
+        message: 'Вы уверены что хотите <b>удалить</b> этот заказ? Сперва убедитесь что заказчик отказался либо заказал не правильно!',
+        confirmText: 'Удалить заказ',
+        cancelText: 'Отмена',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => {
+          this.$toast.open({
+            message: `Заказ <b> ${payload.name}</b> удалён! `,
+            duration: 3000,
+            position: 'is-bottom-left',
+            type: 'is-dark'
+          });
+          this.$store.dispatch("deleteOrder", payload)
+        }
+      })
+    },
+    confirmOrder (payload) {
+      this.$store.dispatch("confirmOrder", payload);
+    },
+    finishOrder (payload) {
+      this.$store.dispatch("finishOrder", payload);
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from './plugins/axios'
-
-
+import axios from "./plugins/axios";
 
 Vue.use(Vuex);
 
@@ -12,27 +10,33 @@ export default new Vuex.Store({
     defaultUrl: "http://192.168.0.51:4000",
     newProduct: {},
     categories: [],
-    promotions: [{
+    promotions: [
+      {
         name: "День Рождения",
-        description: "В день вашего рождения дарим Пиццу-пирог 25 см! Акция действует по промокоду D120 один раз.",
-        image: "http://www.metroves.ru/upload/iblock/e71/e7173f9295275c498c4491536ac1a76c.png",
+        description:
+          "В день вашего рождения дарим Пиццу-пирог 25 см! Акция действует по промокоду D120 один раз.",
+        image:
+          "http://www.metroves.ru/upload/iblock/e71/e7173f9295275c498c4491536ac1a76c.png",
         id: 1,
         date: Date.now()
       },
       {
         name: "Доставим вовремя или за наш счет",
-        description: "Мы придерживаемся золотых правил и всегда следим за тем, чтобы наши заказы доставлялись вовремя, а лучше даже раньше обещанного времени.",
+        description:
+          "Мы придерживаемся золотых правил и всегда следим за тем, чтобы наши заказы доставлялись вовремя, а лучше даже раньше обещанного времени.",
         image: "http://mobile.mypizza.kg:9715/api/ImageFor?id=3115",
         id: 2,
         date: Date.now()
       }
     ],
-    orders: [{
-        name: 'Марсель Мелисов',
+    orders: [
+      {
+        name: "Марсель Мелисов",
         phone: "+996 558 408 448",
         address: "Айни, 89",
         total: 2817,
-        orderList: [{
+        orderList: [
+          {
             product_id: "c90d74e40d7b81368b92b49",
             product_quantity: 1
           },
@@ -43,18 +47,20 @@ export default new Vuex.Store({
           {
             product_id: "5c90d7de40d7b81368b92b4d",
             product_quantity: 1
-          }, {
+          },
+          {
             product_id: "5c90d80e40d7b81368b92b4f",
             product_quantity: 1
           }
         ]
       },
       {
-        name: 'Ffq',
+        name: "Ffq",
         phone: "+996 558 408 448",
         address: "Айни, 89",
         total: 2817,
-        orderList: [{
+        orderList: [
+          {
             product_id: "c90d74e40d7b81368b92b49",
             product_quantity: 1
           },
@@ -65,18 +71,20 @@ export default new Vuex.Store({
           {
             product_id: "5c90d7de40d7b81368b92b4d",
             product_quantity: 1
-          }, {
+          },
+          {
             product_id: "5c90d80e40d7b81368b92b4f",
             product_quantity: 1
           }
         ]
       },
       {
-        name: ' Мasdелисов',
+        name: " Мasdелисов",
         phone: "+996 558 408 448",
         address: "Айни, 89",
         total: 2817,
-        orderList: [{
+        orderList: [
+          {
             product_id: "c90d74e40d7b81368b92b49",
             product_quantity: 1
           },
@@ -87,17 +95,20 @@ export default new Vuex.Store({
           {
             product_id: "5c90d7de40d7b81368b92b4d",
             product_quantity: 1
-          }, {
+          },
+          {
             product_id: "5c90d80e40d7b81368b92b4f",
             product_quantity: 1
           }
         ]
-      }, {
-        name: 'lorem',
+      },
+      {
+        name: "lorem",
         phone: "+996 558 408 448",
         address: "Айни, 89",
         total: 2817,
-        orderList: [{
+        orderList: [
+          {
             product_id: "c90d74e40d7b81368b92b49",
             product_quantity: 1
           },
@@ -108,35 +119,40 @@ export default new Vuex.Store({
           {
             product_id: "5c90d7de40d7b81368b92b4d",
             product_quantity: 1
-          }, {
+          },
+          {
             product_id: "5c90d80e40d7b81368b92b4f",
             product_quantity: 1
           }
         ]
       }
     ],
-    ordersInProcess: [{
-      name: 'lorem',
-      phone: "+996 558 408 448",
-      address: "Айни, 89",
-      total: 2817,
-      orderList: [{
-          product_id: "c90d74e40d7b81368b92b49",
-          product_quantity: 1
-        },
-        {
-          product_id: "5c90d79940d7b81368b92b4b",
-          product_quantity: 1
-        },
-        {
-          product_id: "5c90d7de40d7b81368b92b4d",
-          product_quantity: 1
-        }, {
-          product_id: "5c90d80e40d7b81368b92b4f",
-          product_quantity: 1
-        }
-      ]
-    }]
+    ordersInProcess: [
+      {
+        name: "lorem2",
+        phone: "+996 558 408 448",
+        address: "Айни, 89",
+        total: 2817,
+        orderList: [
+          {
+            product_id: "c90d74e40d7b81368b92b49",
+            product_quantity: 1
+          },
+          {
+            product_id: "5c90d79940d7b81368b92b4b",
+            product_quantity: 1
+          },
+          {
+            product_id: "5c90d7de40d7b81368b92b4d",
+            product_quantity: 1
+          },
+          {
+            product_id: "5c90d80e40d7b81368b92b4f",
+            product_quantity: 1
+          }
+        ]
+      }
+    ]
   },
   getters: {
     getOrders: state => state.orders,
@@ -151,8 +167,7 @@ export default new Vuex.Store({
       state.products.filter(element => element.category === "drinks"),
     getDesserts: state =>
       state.products.filter(element => element.category === "desserts"),
-    getCategories: state =>
-      state.categories,
+    getCategories: state => state.categories
   },
   mutations: {
     ADD_PRODUCT: (state, payload) => {
@@ -170,10 +185,10 @@ export default new Vuex.Store({
       state.promotions.push(payload);
     },
     REMOVE_PRODUCT: (state, payload) => {
-      state.products.splice(state.products.indexOf(payload), 1)
+      state.products.splice(state.products.indexOf(payload), 1);
     },
     REMOVE_PROMOTION: (state, payload) => {
-      state.promotions.splice(state.promotions.indexOf(payload), 1)
+      state.promotions.splice(state.promotions.indexOf(payload), 1);
     },
     EDIT_PRODUCT: (state, payload) => {
       let id = payload._id;
@@ -187,13 +202,17 @@ export default new Vuex.Store({
     },
     EDIT_CATEGORY: (state, payload, formData) => {
       let id = payload._id;
-      let editableCategory = state.categories.find(element => element.id === id);
+      let editableCategory = state.categories.find(
+        element => element.id === id
+      );
       editableCategory.name = payload.name;
       editableCategory.code = payload.code;
     },
     EDIT_PROMOTION: (state, payload) => {
       let id = payload.id;
-      let editablePromotion = state.promotions.find(element => element.id === id)
+      let editablePromotion = state.promotions.find(
+        element => element.id === id
+      );
       editablePromotion.name = payload.name;
       editablePromotion.description = payload.description;
       editablePromotion.image = payload.image;
@@ -206,100 +225,112 @@ export default new Vuex.Store({
     },
     DELETE_CATEGORY: (state, payload) => {
       state.categories.splice(state.categories.indexOf(payload), 1);
+    },
+    REMOVE_ORDER: (state, payload) => {
+      state.orders.splice(state.orders.indexOf(payload), 1);
+    },
+    CONFIRM_ORDER: (state, payload) => {
+      state.ordersInProcess.unshift(payload);
+    },
+    FINISH_ORDER: (state, payload) => {
+      state.ordersInProcess.splice(state.ordersInProcess.indexOf(payload), 1);
     }
   },
   actions: {
-    addPromotion({
-      commit
-    }, payload) {
+    addPromotion({ commit }, payload) {
       commit("ADD_PROMOTION", payload);
     },
-    addProduct({
-      commit
-    }, payload) {
-      console.log('action', payload);
-      axios.post('product', payload).then(res => {
-        console.log('added', res.data);
-      }).catch(err => {
-        console.error(err);
-      })
+    addProduct({ commit }, payload) {
+      console.log("action", payload);
+      axios
+        .post("product", payload)
+        .then(res => {
+          console.log("added", res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
       commit("ADD_PRODUCT", payload);
     },
-    editProduct({
-      commit
-    }, {
-      editedProduct,
-      editedFormData
-    }) {
-      console.log('formData', editedFormData);
-      axios.put('product', editedFormData).then(res => console.log('edited', res)).catch(err => console.error(err.res))
+    editProduct({ commit }, { editedProduct, editedFormData }) {
+      console.log("formData", editedFormData);
+      axios
+        .put("product", editedFormData)
+        .then(res => console.log("edited", res))
+        .catch(err => console.error(err.res));
       commit("EDIT_PRODUCT", editedProduct, editedFormData);
     },
-    editCategory({
-      commit
-    }, payload) {
-      axios.put('category', payload).then(res => console.log('edited catt.', res)).catch(err => console.error(err))
-      commit("EDIT_CATEGORY", payload)
+    editCategory({ commit }, payload) {
+      axios
+        .put("category", payload)
+        .then(res => console.log("edited catt.", res))
+        .catch(err => console.error(err));
+      commit("EDIT_CATEGORY", payload);
     },
-    editPromotion({
-      commit
-    }, payload) {
+    editPromotion({ commit }, payload) {
       commit("EDIT_PROMOTION", payload);
-
     },
-    addCategory({
-      commit
-    }, payload) {
-      commit('ADD_CATEGORY', payload);
-      axios.post('category', payload).then(res => console.log('added cat.', res)).catch(err => console.error(err))
+    addCategory({ commit }, payload) {
+      commit("ADD_CATEGORY", payload);
+      axios
+        .post("category", payload)
+        .then(res => console.log("added cat.", res))
+        .catch(err => console.error(err));
     },
-    deleteCategory({
-      commit
-    }, payload) {
-      axios.delete('category', {
-        data: {
-          _id: payload._id
-        }
-      }).then(res => {
-        console.log('Deleted', payload.name);
-      }).catch(err => console.error(err))
+    deleteCategory({ commit }, payload) {
+      axios
+        .delete("category", {
+          data: {
+            _id: payload._id
+          }
+        })
+        .then(res => {
+          console.log("Deleted", payload.name);
+        })
+        .catch(err => console.error(err));
       commit("DELETE_CATEGORY", payload);
     },
-    deleteProduct({
-      commit
-    }, payload) {
-      axios.delete('product', {
-        data: {
-          id: payload._id
-        }
-      }).then(res => {}).catch(err => console.error(err))
+    deleteProduct({ commit }, payload) {
+      axios
+        .delete("product", {
+          data: {
+            id: payload._id
+          }
+        })
+        .then(res => {})
+        .catch(err => console.error(err));
       commit("REMOVE_PRODUCT", payload);
     },
-    deletePromotion({
-      commit
-    }, payload) {
-      commit("REMOVE_PROMOTION", payload)
+    deletePromotion({ commit }, payload) {
+      commit("REMOVE_PROMOTION", payload);
     },
-    fetchProduct({
-      commit
-    }) {
-
-      axios.get('product')
+    deleteOrder({ commit }, payload) {
+      commit("REMOVE_ORDER", payload);
+    },
+    confirmOrder({ commit }, payload) {
+      commit("CONFIRM_ORDER", payload);
+      commit("REMOVE_ORDER", payload);
+    },
+    finishOrder({ commit }, payload) {
+      commit("FINISH_ORDER", payload);
+    },
+    fetchProduct({ commit }) {
+      axios
+        .get("product")
         .then(response => {
           let products = response.data.products;
-          commit('SET_PRODUCTS', products);
+          commit("SET_PRODUCTS", products);
         })
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
     },
-    fetchCategories({
-      commit
-    }) {
-      axios.get('category')
+    fetchCategories({ commit }) {
+      axios
+        .get("category")
         .then(res => {
           let categories = res.data.products;
-          commit('SET_CATEGORIES', categories)
+          commit("SET_CATEGORIES", categories);
         })
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
     }
   }
 });
